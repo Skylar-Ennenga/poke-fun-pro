fc2fd232d09fccba09d116a5660f4ef939a1f316
Skylar-Ennenga <skylar.ennenga@gmail.com>
2025-10-06 14:42:37 -0600

    tailwind and shadcn intergrated and working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
       This is where the fun begins
+      <Button className="bg-blue-400 hover:bg-amber-400">This is the new button</Button>
     </div>
   );
 }
